7a4d470551569376ff6516363ed48ffc41af7ea5
tomas miranda <tmirandafrontan@gmail.com>
2023-10-16 20:27:52 -0300

    cambios en home y rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import AccessPoint from "./pages/home/AccessPoint.jsx"
-import Login from "./pages/welcomePage/Login.jsx"
-import Register from "./pages/welcomePage/Register.jsx"
-import Search from "./pages/search/Search.jsx"
-import Profile from "./pages/profile/Profile.jsx"
-import Playlists from "./pages/profile/Playlists.jsx"
-import Settings from "./pages/profile/Settings.jsx"
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./pages/home/Home.jsx";
+import "./index.css";
+// import Login from "./pages/welcomePage/Login.jsx";
+// import Register from "./pages/welcomePage/Register.jsx";
+// import Search from "./pages/search/Search.jsx";
+// import Profile from "./pages/profile/Profile.jsx";
+// import Playlists from "./pages/profile/Playlists.jsx";
+// import Settings from "./pages/profile/Settings.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
+  // {
+  //   path: "/",
+  //   element: <WelcomePage />,
+  // },
 
   {
-    path: "/accesspoint",
-    element: <AccessPoint />,
+    path: "/home",
+    element: <Home />,
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/search",
-    element: <Search />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/playlists",
-    element: <Playlists />,
-  },
-  {
-    path: "/settings",
-    element: <Settings />,
-  },
+  // {
+  //   path: "/login",
+  //   element: <Login />,
+  // },
+  // {
+  //   path: "/register",
+  //   element: <Register />,
+  // },
+  // {
+  //   path: "/search",
+  //   element: <Search />,
+  // },
+  // {
+  //   path: "/profile",
+  //   element: <Profile />,
+  // },
+  // {
+  //   path: "/playlists",
+  //   element: <Playlists />,
+  // },
+  // {
+  //   path: "/settings",
+  //   element: <Settings />,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import AccessPoint from "../../components/home/AccessPoint";
+
+function Home() {
+  return (
+    <>
+      <AccessPoint
+        title="Cupido Musical"
+        text="Tus artistas favoritos nunca van a dejarte con el corazon roto"
+      ></AccessPoint>
+    </>
+  );
+}
+
+export default Home;
