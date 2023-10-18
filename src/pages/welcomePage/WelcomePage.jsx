86ef441bda114febe5b5239286ccb4d399dd04cb
H4rdan <javier-pereyra-14@hotmail.com>
2023-10-18 19:08:41 -0300

    commit de rama welcomepage

--- src/pages/welcomePage/WelcomePage.jsx
+++ src/pages/welcomePage/WelcomePage.jsx
@@ -1,9 +1,33 @@
-import "../styles/WelcomePage.css";
+import "../../styles/WelcomePage.css";
 
 function WelcomePage() {
     return (
         <>
+            <div className="login-background">
 
+            </div>
+            <div>
+                <img src="../Logotipo.svg" alt="" />
+            </div>
+
+            <h1>Música a medida.</h1>
+
+            <div>
+                <button>
+                    Registrarse Gratis
+                </button>
+                <button>
+                    <img src="../google-logo.svg" alt="" />
+                    Continuar con google
+                </button>
+                <button>
+                    <img src="../apple-logo.svg" alt="" />
+                    Continuar con Apple
+                </button>
+            </div>
+            <button>
+                Iniciar Sesión
+            </button>
         </>
     );
 }
