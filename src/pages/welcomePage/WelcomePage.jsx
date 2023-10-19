a1290a68aa6ea6cced9b040f6755ff6b0d5816b1
H4rdan <javier-pereyra-14@hotmail.com>
2023-10-19 19:26:02 -0300

    actualización welcome page

--- src/pages/welcomePage/WelcomePage.jsx
+++ src/pages/welcomePage/WelcomePage.jsx
@@ -3,31 +3,39 @@ import "../../styles/WelcomePage.css";
 function WelcomePage() {
     return (
         <>
-            <div className="login-background">
+            <div className="welcome-page-container">
+                <div className="welcome-background">
 
-            </div>
-            <div>
-                <img src="../Logotipo.svg" alt="" />
+                </div>
+                <div className="welcome-logo">
+                    <div>
+                        <img src="../Logotipo.svg" alt="" />
+                    </div>
+
+                    <p>Música a medida.</p>
+
+                </div>
+                <div className="welcome-buttons-container">
+                    <div className="welcome-buttons">
+                        <button className="orange-button">
+                            Registrarse Gratis
+                        </button>
+                        <button className="border-trans-button">
+                            <img src="../google-logo.svg" alt="" />
+                            Continuar con google
+                        </button>
+                        <button className="border-trans-button">
+                            <img src="../apple-logo.svg" alt="" />
+                            Continuar con Apple
+                        </button>
+                    </div>
+                    <button className="trans-button">
+                        Iniciar Sesión
+                    </button>
+                </div>
+                <div className="bottom-gradient"></div>
             </div>
 
-            <h1>Música a medida.</h1>
-
-            <div>
-                <button>
-                    Registrarse Gratis
-                </button>
-                <button>
-                    <img src="../google-logo.svg" alt="" />
-                    Continuar con google
-                </button>
-                <button>
-                    <img src="../apple-logo.svg" alt="" />
-                    Continuar con Apple
-                </button>
-            </div>
-            <button>
-                Iniciar Sesión
-            </button>
         </>
     );
 }
