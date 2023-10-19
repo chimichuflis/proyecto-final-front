df78d6a5a5a7aaccb189ee179cea29b32260f7a9
silver <silver@sildrid.com>
2023-10-18 21:51:49 -0300

    trabajando en el cupido musical

--- src/pages/musicMatch/MusicMatch.jsx
+++ src/pages/musicMatch/MusicMatch.jsx
@@ -3,10 +3,17 @@ import MatchCarousel from "./MatchCarousel.jsx";
 import "../../styles/MusicMatch.css";
 
 export default function(){
-  return(<>
-    <PageTitle title="Cupido Musical"/>
-    <MatchCarousel next="eye.svg" toMatch="loader.svg" previous="back.svg"/>
-    <button>Crear Playlist</button>
-
-  </>)
+  const  artistArr = [
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpeg"
+  ]
+  return(
+    <div className="music-match">
+      <PageTitle title="Cupido Musical"/>
+      <MatchCarousel next={`artists/${artistArr[0]}`} toMatch={`artists/${artistArr[1]}`} previous={`artists/${artistArr[2]}`}/>
+      <button>Crear Playlist</button>
+    </div>)
 }
